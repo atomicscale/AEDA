b0d109a274980cb7bc48e1a4de49b4ebfc196532
joaofidalgo <up201303098@fe.up.pt>
2014-11-01 19:27:40 +0000

    socios, falta por data(caso nao paguem ate dia 8)

--- Socio.cpp
+++ Socio.cpp
@@ -1,9 +1,30 @@
 #include "Socio.h"
+#include <vector>
+#include <string>
 
 using namespace std;
 
+Socio::Socio(string nome, float quotaT, int nr_modalidades) {
+	this->nome = nome;
+	this->quotaT = quotaT;
+	this->nr_modalidades = nr_modalidades;
+}
 
-Socio::Socio(string nome, int cota){
-	this->nome=nome;
-	this->cota = cota;
+float Socio::getQuotaT() {
+	for (unsigned int i = 0; i < modalidS.size(); i++) {
+		quotaT += modalidS[i]->getQuota();
+	}
+	if (modalidS.size() > 3)
+		return quotaT * 0.95;
+		return quotaT;
+}
+
+void Socio::imprime(){
+	cout<<"Nome: "<< nome << endl;
+	cout<<"Quota/mês: " << quotaT << endl;
+	cout<<"Modalidade(s) pagante(s): "<<endl;
+	for(unsigned int i=0; i<modalidS.size();i++){
+		 cout<< modalidS[i]->getNome()<< endl;
+		 i++;
+	}
 }
--- Socio.h
+++ Socio.h
@@ -2,18 +2,25 @@
 #define SOCIO_H_
 
 #include <string>
+#include <vector>
 
 using namespace std;
 
 class Socio {
 	string nome;
-	int cota;
+	float quotaT;
+	int nr_modalidades;
+	vector<Modalidade*> modalidS;
 public:
-	Socio(string nome, int cota);
+	Socio(string nome, float quotaT, int nr_modalidades);
 	string getNome() {return nome;};
-	int getCota() {return cota;};
+	int getnrModalidades() {return modalidS.size();};
+	float getQuotaT();
+	void imprime();
 };
 
 
-
 #endif /* SOCIO_H_ */
+
+
+/* FALTA DATA PARA QUANDO SE ATRASA A PAGAR*/
