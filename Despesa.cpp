6bc17431ccead37563d7d80707f3c9e9641f542f
eduardo <edmartins_10@hotmail.com>
2015-01-04 00:30:17 +0000

    Remover Nhanha

--- Despesa.cpp
+++
@@ -1,9 +0,0 @@
-#include "Despesa.h"
-
-using namespace std;
-
-Despesa::Despesa(int dia, int mes, int ano,int valor, string tipo){
-	this->data = new Data(dia,mes,ano);
-	this->total = valor;
-	this->tipo = tipo;
-}
